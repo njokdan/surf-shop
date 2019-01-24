98efceda8295c3a7cccb1d9cc495039853e48831
Jacobo Martinez <jacobojavier98@gmail.com>
2019-01-24 14:57:35 -0400

    add index controllers

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
-const createError   = require('http-errors');
-const express       = require('express');
-const path          = require('path');
-const cookieParser  = require('cookie-parser');
-const logger        = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const session = require('express-session');
+const passport = require('passport');
+const User = require('./models/user');
 
-const indexRouter   = require('./routes/index');
-const postsRouter   = require('./routes/posts');
+// Require routes
+const indexRouter = require('./routes/index');
+const postsRouter = require('./routes/posts');
 const reviewsRouter = require('./routes/reviews');
 
 const app = express();
+
+// MongoDB connect
+mongoose.connect('mongodb://localhost:27017/surf-shop');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,14 +30,27 @@ app.use(express.urlencoded({
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// routes
+// Passport and Sessions Setup
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true
+}))
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+// Mount routes
 app.use('/', indexRouter);
 app.use('/posts', postsRouter);
 app.use('/posts/:id/reviews', reviewsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-	next(createError(404));
+    next(createError(404));
 });
 
 // error handler
