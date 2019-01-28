fde409dc42e4fca42686de9bc27cc6645bfa07fe
Jacobo Martinez <jacobojavier98@gmail.com>
2019-01-28 15:18:15 -0400

    add cloudinary, multer packages, add multer config to posts routes

--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const upload = multer({'dest': 'uploads/'})
 const { asyncErrorHandler } = require('../middlewares/index');
 const { 
     postIndex,
@@ -18,7 +20,7 @@ router.get('/', asyncErrorHandler(postIndex));
 router.get('/new', postNew);
 
 /* POST create post - /posts */
-router.post('/', asyncErrorHandler(postCreate));
+router.post('/', upload.array('images', 4), asyncErrorHandler(postCreate));
 
 /* GET show post - /posts/:id */
 router.get('/:id', asyncErrorHandler(postShow));
