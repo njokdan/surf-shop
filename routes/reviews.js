eb5ce16d638f11abd6a036387847aa322cc9ac0c
Jacobo Martinez <jacobojavier98@gmail.com>
2019-02-22 19:05:16 -0400

    add create reviews controller and show reviews in show post

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,29 +1,19 @@
 const express = require('express');
 const router = express.Router({mergeParams: true}); // Allow to get the url :placeholder from the route confg in app.js
+const { asyncErrorHandler } = require('../middlewares/index');
+const {
+    reviewCreate,
+    reviewUpdate,
+    reviewDestroy
+} = require('../controllers/reviews');
 
-/* GET posts index - /posts/:id/reviews */
-router.get('/', (req, res) => {
-    res.send('INDEX /reviews');
-});
-
-/* POST create post - /posts/:id/reviews */
-router.post('/', (req, res) => {
-    res.send('CREATE /posts/:id/reviews');
-});
-
-/* GET edit post - /posts/:id/reviews/:review_id/edit */
-router.get('/:review_id/edit', (req, res) => {
-    res.send('EDIT /posts/:id/reviews/:review_id/edit');
-});
+/* POST reviews post - /posts/:id/reviews */
+router.post('/', asyncErrorHandler(reviewCreate));
 
 /* PUT update post - /posts/:id/reviews/:review_id */
-router.put('/:review_id', (req, res) => {
-    res.send('UPDATE /posts/:id/reviews/:review_id');
-});
+router.put('/:review_id', asyncErrorHandler(reviewUpdate));
 
 /* DELETE destroy post - /posts/:id/reviews/:review_id */
-router.delete('/:review_id', (req, res) => {
-    res.send('DESTROY /posts/:id/reviews/:review_id');
-});
+router.delete('/:review_id', asyncErrorHandler(reviewDestroy));
 
 module.exports = router;
